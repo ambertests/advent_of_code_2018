b6d168aba1d27cdaf40f5c7d5a2288eeae149fa3
Amber Race <amber.race@outlook.com>
2018-12-06 05:39:52 +0300

    Optimize Day 3 solution so it only uses one loop instead of two

--- aoc_day03.py
+++ aoc_day03.py
@@ -5,7 +5,9 @@ input = [x.strip() for x in open('input/input03.txt').readlines()]
 
 fabric = [[0 for y in range(1000)] for x in range(1000)]
 overlap = 0
+no_overlap_ids = []
 for coord in input:
+    has_overlap = False
     # #35 @ 923,270: 12x22
     id = int(coord[1:coord.index(' @ ')])
     x_offset = int(coord[coord.index(' @ ') + 3:coord.index(',')])
@@ -16,27 +18,17 @@ for coord in input:
         for y in [j+y_offset for j in range(height)]:
             if fabric[x][y] == 0: fabric[x][y] = id
             else:
-                # mark overlap with a -1 so it is only counted once
+                has_overlap = True
                 if fabric[x][y] > 0:
+                    if fabric[x][y] in no_overlap_ids: 
+                        no_overlap_ids.remove(fabric[x][y])
+                    # mark overlap with a -1 so it is only counted once
                     fabric[x][y] = -1
                     overlap = overlap + 1
+    if not has_overlap: no_overlap_ids.append(id)
 
-print('Solution 3.1: ', overlap)
+print('Solution 3.1:', overlap)
+print('Solution 3.2:', no_overlap_ids)
 
-for coord in input:
-    # yes, it is lazy to copy/paste the string splitting...
-    id = int(coord[1:coord.index(' @ ')])
-    x_offset = int(coord[coord.index(' @ ') + 3:coord.index(',')])
-    y_offset = int(coord[coord.index(',')+1:coord.index(':')])
-    width = int(coord[coord.index(': ')+2:coord.index('x')])
-    height = int(coord[coord.index('x')+1:])
-    overlapped = False
-    for x in [i+x_offset for i in range(width)]:
-        for y in [j+y_offset for j in range(height)]:
-            if fabric[x][y] == -1:
-                overlapped = True
-                break
-        if overlapped: break
-    if not overlapped: print('Solution 3.2: ', id)
 
 
